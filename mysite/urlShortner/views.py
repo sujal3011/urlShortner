4d11b71385df88e031ad89b5aa814d3f931a5ed6
Sivam2313 <sivamdas7@gmail.com>
2023-02-06 13:21:30 +0530

    views

--- mysite/urlShortner/views.py
+++ mysite/urlShortner/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Links
+from django.shortcuts import redirect
 import json
+import random
+import string
 
 def home(request):
     context = {}
+    if request.method == 'POST':
+        result = request.POST['slug']
+        if result is None:
+            result = ''.join(
+                (random.choice(string.ascii_lowercase) for x in range(8)))
+        link = Links.objects.create(urls = request.POST['urls'],slug = result)
+        return redirect(link.urls)
     return render(request,'index.html',context)
+
+
+def redir(request,slugs):
+    data = Links.objects.get(slug = slugs)
+    return redirect(data.urls)
