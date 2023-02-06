ec3fe5fd60091073d386ffcb605a306eb84cd27d
Sivam2313 <sivamdas7@gmail.com>
2023-02-06 12:51:46 +0530

    initial

--- mysite/urlShortner/views.py
+++ mysite/urlShortner/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+def home(request):
+    context = {}
+    return render(request,'index.html',context)
