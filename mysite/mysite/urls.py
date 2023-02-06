4001c617450270d8d6149384512f691421461b76
Sivam2313 <sivamdas7@gmail.com>
2023-02-06 13:48:06 +0530

    final commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('urls/', include("urlShortner.urls")),
+    path('u/', include("urlShortner.urls")),
 ]
