f44f8b4da28f00852e0ee241ac1d40d587194572
Vedant Singh <vedu8051@gmail.com>
2023-02-07 15:14:56 +0530

    finallly

---
+++ urlShortner/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .models import Links
+from django.shortcuts import redirect
+import json
+import random
+import string
+
+def home(request):
+    context = {}
+    if request.method == 'POST':
+        result = request.POST['slug']
+        t=''
+        if result is t:
+            result = ''.join(
+                (random.choice(string.ascii_letters) for x in range(8)))
+        print(result)
+        link = Links.objects.create(urls = request.POST['urls'],slug = result)
+        link.save()
+        context={
+            "link":link
+            }
+        return render(request,'pp.html',context)
+    return render(request,'index.html',context)
+
+
+def redir(request,slugs):
+    data = Links.objects.get(slug = slugs)
+    return HttpResponseRedirect(data.urls)
